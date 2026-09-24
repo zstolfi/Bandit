21574bc1e53416243a724cd3f7a534dfa8a0aeb9
zstolfi <zander5645@gmail.com>
2026-09-24 18:09:42 -0500

    Start creating coordinate vector library

--- src/math.hh
+++ src/math.hh
@@ -1,6 +1,115 @@
 #pragma once
 #include "util.hh"
 
+/* ~~ Linear Algebra ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+// for 3D graphics
+
+class Coord3 {
+protected:
+	std::array<double, 3> data_m {};
+
+public:
+	Coord3(): data_m{0, 0, 0} {}
+	Coord3(double x, double y, double z): data_m{x, y, z} {}
+	auto operator<=>(Coord3 const&) const = default;
+
+	// Getters    Ex: value = position.x();
+	double x() const { return data_m[0]; }
+	double y() const { return data_m[1]; }
+	double z() const { return data_m[2]; }
+
+	// Setters    Ex: position.x(value);
+	// This syntax is strange but it allows us to keep a close eye on our data.
+	// If we were to send out references, it means the user could de-normalize
+	// our data at will.
+	void x(double val) { data_m[0] = val; normalize(); }
+	void y(double val) { data_m[1] = val; normalize(); }
+	void z(double val) { data_m[2] = val; normalize(); }
+
+	// Properties
+	double length2() const {
+		return
+			data_m[0] * data_m[0] +
+			data_m[1] * data_m[1] +
+			data_m[2] * data_m[2]
+		;
+	}
+
+protected:
+	virtual Coord3& normalize() { /* Do nothing. */ return *this; }
+};
+
+class Norm3: public Coord3 {
+public:
+	Norm3(): Coord3{1, 0, 0} {}
+	Norm3(Coord3 c): Coord3{c} { normalize(); }
+	Norm3(double x, double y, double z): Coord3{x, y, z} { normalize(); }
+
+private:
+	Coord3& normalize() override {
+		bool strict = std::is_constant_evaluated();
+		if (length2() != 0.0) {
+			double inv = std::pow(length2(), -0.5);
+			data_m[0] *= inv;
+			data_m[1] *= inv;
+			data_m[2] *= inv;
+			return *this;
+		}
+		// Defining an invalid normal at compile time is definitely an error.
+		if (strict) throw std::domain_error {"Norm3 division by 0"};
+		// Otherwise it's best to just keep our state valid.
+		return *this = {};
+	}
+};
+
+class Ray3 {
+	Coord3 origin_m {};
+	Norm3 direction_m {};
+
+public:
+	// Getters
+	Coord3 const& origin() const { return origin_m; }
+	Norm3 const& direction() const { return direction_m; }
+
+	// Setters
+	void origin(Coord3 c) { origin_m = c; }
+	void direction(Norm3 n) { direction_m = n; }
+};
+
+class Plane3 {
+	Coord3 origin_m {};
+	Norm3 direction_m {};
+
+public:
+	// Getters
+	Coord3 const& origin() const { return origin_m; }
+	Norm3 const& direction() const { return direction_m; }
+
+	// Setters
+	void origin(Coord3 c) { origin_m = c; normalize(); }
+	void direction(Norm3 n) { direction_m = n; }
+
+	// Properties
+	double length()
+
+private:
+	normalize() {
+		double dist =
+			origin_m[0] * direction_m[0] +
+			origin_m[1] * direction_m[1] +
+			origin_m[2] * direction_m[2]
+		;
+		origin_m = {
+			direction_m[0] * dist,
+			direction_m[1] * dist,
+			direction_m[2] * dist,
+		};
+	}
+}
+
+/* ~~ Group Theory ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+// for puzzle logic
+
 class Permutation {
 	using Index = unsigned;
 	using Cycle = std::vector<Index>;
